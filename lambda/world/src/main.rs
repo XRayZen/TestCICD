0902711a902accf5be5598a657c8669bb26eae57
Matagi <31905238+XRayZen@users.noreply.github.com>
2023-05-24 13:18:10 +0900

    RustWorldとHelloドッカーファイルを変更

--- lambda/world/src/main.rs
+++ lambda/world/src/main.rs
@@ -1,4 +1,4 @@
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response, aws_lambda_events::serde_json::ser};
 
 /// 機能の本体となります。
 /// その中にコードを書く。
@@ -7,7 +7,7 @@ use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
 
     let message = format!("Hello this is an AWS Lambda HTTP request");
-    
+    // let ddd= ser::to_string(&message).unwrap();
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
     let resp = Response::builder()
