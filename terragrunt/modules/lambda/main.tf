5577bf4a8d4e959f19501228797e9640e3693054
Matagi <31905238+XRayZen@users.noreply.github.com>
2023-05-27 03:15:25 +0900

    fix lambda image_uri

--- terragrunt/modules/lambda/main.tf
+++ terragrunt/modules/lambda/main.tf
@@ -7,7 +7,7 @@ resource "aws_lambda_function" "lambda_func_" {
   role          = aws_iam_role.lambda_role.arn
 
   package_type = "Image"
-  image_uri    = "${var.repo_url}:${var.image_tag}"
+  image_uri    = "${var.repo_url}/:${var.image_tag}"
 
   # vpc_config {
   #   subnet_ids         = var.subnet_ids
