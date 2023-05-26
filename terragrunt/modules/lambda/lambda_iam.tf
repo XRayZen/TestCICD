81125c608e723a901c74a062ad15af509ceb0369
Matagi <31905238+XRayZen@users.noreply.github.com>
2023-05-27 06:41:48 +0900

    fix var.managed_policy_arns

--- terragrunt/modules/lambda/lambda_iam.tf
+++ terragrunt/modules/lambda/lambda_iam.tf
@@ -2,17 +2,7 @@
 resource "aws_iam_role" "lambda_role" {
   name               = "${var.lambda_function_name}-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_role_policy.json
-  managed_policy_arns = [
-    # Lambda関数がCloudWatch Logsにログを書き込むための最低限の権限を提供します。
-    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
-    # Lambda関数がVPC内のリソースにアクセスしながら実行するための最低限の権限（ネットワークインターフェースの作成、記述、削除、CloudWatch Logsへの書き込み権限）を提供します。
-    "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole",
-    # Amazon ECR に対する読み取り専用アクセスを付与
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
-    # APIGW用
-    "arn:aws:iam::aws:policy/AmazonAPIGatewayInvokeFullAccess",
-  ]
+  managed_policy_arns = var.managed_policy_arns
 }
 
 data "aws_iam_policy_document" "lambda_role_policy" {
