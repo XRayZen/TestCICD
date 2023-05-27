c973e1f190611ba39275f95bd58dbd93c166fd19
Matagi <31905238+XRayZen@users.noreply.github.com>
2023-05-27 15:05:12 +0900

    add lambda_hello

--- lambda/world/src/main.rs
+++ lambda/world/src/main.rs
@@ -1,11 +1,12 @@
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response, aws_lambda_events::serde_json::ser};
+use lambda_http::{
+    aws_lambda_events::serde_json::ser, run, service_fn, Body, Error, Request, RequestExt, Response,
+};
 
 /// 機能の本体となります。
 /// その中にコードを書く。
 /// 以下のURLにコード例があります：
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
-
     let message = format!("Hello this is an AWS Lambda HTTP request");
     // let ddd= ser::to_string(&message).unwrap();
     // Return something that implements IntoResponse.
@@ -27,6 +28,6 @@ async fn main() -> Result<(), Error> {
         // disabling time is handy because CloudWatch will add the ingestion time.
         .without_time()
         .init();
-    
+
     run(service_fn(function_handler)).await
 }
