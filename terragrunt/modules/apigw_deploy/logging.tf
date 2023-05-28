cc153967ba3b5f46b16c9c65b4630b46763c4537
Matagi <31905238+XRayZen@users.noreply.github.com>
2023-05-28 17:08:24 +0900

    fix method-path

--- terragrunt/modules/apigw_deploy/logging.tf
+++ terragrunt/modules/apigw_deploy/logging.tf
@@ -1,8 +1,11 @@
 
+# API Gatewayのログ出力設定
+# https://docs.aws.amazon.com/ja_jp/apigateway/latest/developerguide/set-up-logging.html
+# TF公式: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/api_gateway_stage
 resource "aws_api_gateway_method_settings" "api_gw_method_settings" {
   rest_api_id = var.api_gw_rest_api_id
   stage_name  = aws_api_gateway_stage.api_stage.stage_name
-  method_path = var.api_gw_resource_path
+  method_path = "*/*"
 
   settings {
     metrics_enabled = true
