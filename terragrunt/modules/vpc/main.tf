8753ca0e6d43976eed1934aee49c0a496e71f19f
Matagi <31905238+XRayZen@users.noreply.github.com>
2023-05-31 04:55:00 +0900

    fix modules

--- terragrunt/modules/vpc/main.tf
+++ terragrunt/modules/vpc/main.tf
@@ -6,12 +6,12 @@ module "vpc" {
   cidr = var.cidr
 
   azs             = var.availability_zones
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
   # NATゲートウェイを使う場合はtrueにする
-  enable_nat_gateway     = false
-  single_nat_gateway     = false
-  one_nat_gateway_per_az = false
+  enable_nat_gateway     = var.enable_nat_gateway
+  single_nat_gateway     = var.single_nat_gateway
+  one_nat_gateway_per_az = var.one_nat_gateway_per_az
 }
 
 # ラムダ関数をVPC内で実行するためには、VPCのサブネットを指定する必要があるので用意する
